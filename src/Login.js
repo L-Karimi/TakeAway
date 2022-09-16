499c2e3e8a176e3600adba3f5cdd4b8f6df589a8
L-Karimi <luciahkei101@gmail.com>
2022-09-17 00:57:58 +0300

    added login and signup forms

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,59 @@
+import React from "react";
 
+const Login = () => {
+  return (
+    <div>
+      <a className="hiddenanchor" id="signin"></a>
 
-const Login=()=>{
+      <div className="login_wrapper">
+        <div className="animate form login_form">
+          <section className="login_content">
+            <form id="login_form">
+              <h1>
+                <span className="display-5 text-success font-weight-bold">
+                  My Login
+                </span>
+                <span className="display-5 text-info font-italic">
+                  Experience
+                </span>
+              </h1>
+              <div>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="PhoneNumber"
+                  required={true}
+                  name="phonenumber"
+                />
+              </div>
+              <div>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  required={true}
+                  name="password"
+                />
 
+                <p style={{ color: "red" }}></p>
+              </div>
+              <div>
+                <button type="submit" className="btn btn-info form-control">
+                  Log in
+                </button>
+              </div>
 
+              <div className="clearfix"></div>
+              <div>
+                <p>©2022 All Rights Reserved.</p>
+              </div>
+              <div className="separator"></div>
+            </form>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-}
-export default Login
+export default Login;
--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,13 @@
 
+import React from 'react'
 
 const Profile=()=>{
-
+    
+      
+    return(
+        <div className="">
+          
+        </div>
+    )
 }
 export default Profile
