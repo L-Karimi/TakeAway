b668433d436cc371639646c588b6523175db7a32
L-Karimi <luciahkei101@gmail.com>
2022-09-17 14:43:12 +0300

    added profile

--- src/Profile.js
+++ src/Profile.js
@@ -5,9 +5,35 @@ const Profile=()=>{
     
       
     return(
-        <div className="">
-          <h1> Profile</h1>
-        </div>
+        <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Profile</h3>
+            <div className="form-group mt-3">
+              <label>First Name</label>
+              <input
+                type="text"
+                className="form-control mt-1"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Last Name</label>
+              <input
+                type="text"
+                className="form-control mt-1"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Phone number</label>
+              <input
+                type="number"
+                className="form-control mt-1"
+              />
+            </div>
+          </div>
+
+        </form>
+      </div>
     )
 }
 export default Profile
